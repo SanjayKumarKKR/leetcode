8950515223e060ec20e18d3b93222a3f1e395802
SanjayKumarKKR <39922507+SanjayKumarKKR@users.noreply.github.com>
2021-04-01 21:32:39 +0530

    Time: 0 ms (100.00%), Space: 2 MB (17.76%) - LeetHub

---
+++ number-of-good-pairs/number-of-good-pairs.go
@@ -0,0 +1,11 @@
+func numIdenticalPairs(nums []int) int {
+    ans:=0
+    for i:=0;i<len(nums);i++{
+        for j:=i+1;j<len(nums);j++{
+            if(nums[i]==nums[j]){
+                ans++
+            }
+        }
+    }
+    return ans
+}
