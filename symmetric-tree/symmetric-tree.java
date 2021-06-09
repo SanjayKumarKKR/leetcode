df9fe11e6856f0b0024de6fa570365d558400c88
SanjayKumarKKR <39922507+SanjayKumarKKR@users.noreply.github.com>
2021-06-09 14:14:17 +0530

    Time: 0 ms (100.00%), Space: 36.9 MB (65.18%) - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -16,18 +16,16 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root==null) return true;
+        
         return symmetric(root.left,root.right);
     }
-    
-    boolean symmetric(TreeNode left,TreeNode right){
+    public boolean symmetric(TreeNode left, TreeNode right){
         if(left==null && right==null){
             return true;
-        }
-        if(left!=null && right!=null && left.val==right.val){
-            return symmetric(left.left,right.right) && symmetric(left.right,right.left);
+        }else if(left!=null && right!=null && left.val==right.val){
+            return symmetric(left.right,right.left) && symmetric(right.right,left.left);
         }else{
             return false;
         }
-        
     }
 }
