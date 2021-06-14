37784f1530460822ee82b6cb132c13c3fcb4e391
SanjayKumarKKR <39922507+SanjayKumarKKR@users.noreply.github.com>
2021-06-14 18:52:13 +0530

    Time: 1 ms (98.25%), Space: 38.7 MB (56.57%) - LeetHub

---
+++ reverse-bits/reverse-bits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int reverse=0;
+        for(int i=0;i<32;i++){
+            int bit = n & 1;
+            n=n>>1;
+            reverse = reverse | bit;
+            if(i<31){
+                reverse = reverse<<1;
+            }
+        }
+        return reverse;
+    }
+}
