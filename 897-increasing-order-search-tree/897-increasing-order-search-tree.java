878e757cc328a519372cd7922d1bb479a5dd52df
SanjayKumarKKR <39922507+SanjayKumarKKR@users.noreply.github.com>
2022-01-26 13:55:02 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (29.00%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -14,18 +14,22 @@
  * }
  */
 class Solution {
-    List<TreeNode> list ;
+    // List<TreeNode> list ;
+    TreeNode curr;
     public TreeNode increasingBST(TreeNode root) {
         if(root==null){return null;}
-        list = new ArrayList<>();
+        // list = new ArrayList<>();
+        TreeNode res = new TreeNode(0);
+        curr = res;
         inorder(root);
-        for(int i=0;i<list.size()-1;i++){
-            list.get(i).right=list.get(i+1);
-            list.get(i).left =null;
-        }
-        list.get(list.size()-1).right=null;
-        list.get(list.size()-1).left =null;
-        return list.get(0);
+        return res.right;
+        // for(int i=0;i<list.size()-1;i++){
+        //     list.get(i).right=list.get(i+1);
+        //     list.get(i).left =null;
+        // }
+        // list.get(list.size()-1).right=null;
+        // list.get(list.size()-1).left =null;
+        // return list.get(0);
     }
     
     public void inorder(TreeNode root){
@@ -34,7 +38,10 @@ class Solution {
         }        
         
         inorder(root.left);
-        list.add(root);
+        // list.add(root);
+        curr.right = root;
+        root.left =null;
+        curr = root;
         inorder(root.right);
     }
 }
